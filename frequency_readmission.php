e327d1078776e323444114928b029a279fe05b9d
16N1K4 <50745340+16N1K4@users.noreply.github.com>
2019-05-26 01:40:51 +0800

    Replaced diabetic_data.php
    This is a more streamlined version of tallying the values in our data set. Use it as the basis for the other graphs.

---
+++ frequency_readmission.php
@@ -0,0 +1,49 @@
+<?php
+
+// Initialize values
+$server = "127.0.0.1";
+$username = "root";
+$password = "password";
+$database = "canvasjs_db";
+$readmitted_no = 0;
+$readmitted_less30 = 0;
+$readmitted_more30 = 0;
+
+// Establish connection
+$connect = mysqli_connect($server, $username, $password, $database);
+
+// Check connection
+if($connect === false){
+	echo "ERROR";
+}
+
+// Fetch all "no readmissions" in table
+$sql = "SELECT * FROM readmitted WHERE readmitted = 0";
+
+// Execute query
+$exec = mysqli_query($connect, $sql);
+
+// Count occurrences
+$readmitted_no = mysqli_num_rows($exec);
+
+// Fetch all "readmissions greater than 30 days" in table
+$sql = "SELECT * FROM readmitted WHERE readmitted = 30";
+
+// Execute query
+$exec = mysqli_query($connect, $sql);
+
+// Count occurrences
+$readmitted_more30 = mysqli_num_rows($exec);
+
+// Fetch all "readmissions less than 30 days" in table
+$sql = "SELECT * FROM readmitted WHERE readmitted = -30";
+
+// Execute query
+$exec = mysqli_query($connect, $sql);
+
+// Count occurrences
+$readmitted_less30 = mysqli_num_rows($exec);
+
+mysqli_close($connect);
+
+?>
