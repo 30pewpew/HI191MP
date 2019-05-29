7cb00238d0e13e9b2233fa31a96c0a0425b79dce
16N1K4 <50745340+16N1K4@users.noreply.github.com>
2019-05-29 22:11:53 +0800

    Minor edit to text, removed extraneous parts

--- index.php
+++ index.php
@@ -1,29 +1,10 @@
 <?php include 'header.php'; ?>
 <?php include 'sidebar.php'; ?>
 <?php include 'content.php'; ?>
-<h1>HI 191 MP</h1>
-<div id="chartContainer"></div>
+<h1>HI 191 MP: Exploratory Data Analysis</h1>
+<h3>Select graph on sidebar menu to begin</h3>
 
 
 
-<script type="text/javascript">
-
-    $(function () {
-        var chart = new CanvasJS.Chart("chartContainer", {
-            theme: "theme2",
-            animationEnabled: true,
-            title: {
-                text: "Select a graph from the sidebar to view data."
-            },
-            data: [
-            {
-                type: "column",                
-                dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-            }
-            ]
-        });
-        chart.render();
-    });
-</script>
 
 <?php include 'footer.php'; ?>
